2b9cf2704b280083dd1c2abbf68e6efdd6194e10
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-08-28 18:41:19 -0700

    App running, Navbar styling complete

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000!')
+})
